d3ee1a814f57d9ea8a834f6572d9960e918f787d
wkdalswns0427 <pawpaw0427@gmail.com>
2024-04-12 00:48:17 +0900

    update with cmdvel

--- arduino_dev/main/main.ino
+++ arduino_dev/main/main.ino
@@ -3,17 +3,28 @@
 #include <std_msgs/UInt8.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Empty.h>
+#include <geometry_msgs/Twist.h>
 
+// ROS usage
 ros::NodeHandle n;
+std_msgs::String str_msg;
+char heartbeat[18] = "arduino heartbeat";
 
+// Subscriber Callbacks
 void numberCallback(const std_msgs::UInt8& msg){
   n.loginfo(msg.data);
 }
+void cmdvelCallback(onst geometry_msgs::Twist& cmdVel){
+  n.loginfo(cmdVel.linear.x);
+  n.loginfo(cmdVel.angular.z);
+}
 
+// SUbscriber Declaration
+ros::Subscriber<geometry_msgs::Twist> subCmdVel("cmd_vel", &cmdvelCallback );
 ros::Subscriber<std_msgs::UInt8> sub("hex", &numberCallback); //Empty
-std_msgs::String str_msg;
+
+// Publisher Declaration
 ros::Publisher rosduino("rosduino", &str_msg);
-char heartbeat[18] = "arduino heartbeat";
 
 
 void setup()
@@ -26,6 +37,7 @@ void setup()
   n.initNode();
   n.advertise(rosduino);
   n.subscribe(sub);
+  n.subscribe(subCmdVel);
 
   // Initial Motor Check should be removed for action
   fb_control(0, 250);
