29966b053ba68ebc4ef45f011b991631e87f7f68
hyunjin <meat124@naver.com>
2024-05-17 04:33:26 +0900

    Fix : move_base navigation fix complete

---
+++ cartrasche/cartrasche_navigation/scripts/stop_vel_pub.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Int32
+
+class StopVel:
+    def __init__(self):
+        rospy.init_node('stop_vel_node')
+        self.cmd_vel_sub = rospy.Subscriber("/cmd_vel", Twist, self.stop_vel_callback)
+        self.stop_vel_pub = rospy.Publisher("/stop_vel", Int32, queue_size=1)
+
+    def stop_vel_callback(self, data):
+        if data.linear.x == 0.0 and data.angular.z == 0.0:
+            rate = rospy.Rate(5)  # 5 Hz
+            stop_vel = Int32(1)
+            for _ in range(5):
+                self.stop_vel_pub.publish(stop_vel)
+                rate.sleep()
+
+    def run(self):
+        rospy.spin()
+
+if __name__ == "__main__":
+    stop_vel = StopVel()
+    stop_vel.run()
