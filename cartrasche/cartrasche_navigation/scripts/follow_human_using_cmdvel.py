254510e8dcf621769fede7a271566ff7e99168db
hyunjin <meat124@naver.com>
2024-05-30 07:14:40 +0900

    Fix : fix camera matrix

--- cartrasche/cartrasche_navigation/scripts/follow_human_using_cmdvel.py
+++ cartrasche/cartrasche_navigation/scripts/follow_human_using_cmdvel.py
@@ -27,8 +27,8 @@ class HumanFollower:
         self.last_center_pixel_time = rospy.Time.now()
         self.timeout_duration = rospy.Duration(1.5)  # 1.5 second timeout
 
-        self.focal_length_x = rospy.get_param('~human_follower/focal_length_x', 1036.51263)
-        self.image_width = rospy.get_param('~human_follower/image_width', 1280)
+        self.focal_length_x = rospy.get_param('~human_follower/focal_length_x', 271.848301)
+        self.image_width = rospy.get_param('~human_follower/image_width', 640)
         self.camera_fov = 2 * math.atan2(self.image_width / 2, self.focal_length_x)  # Field of view of the camera
 
     def center_pixel_callback(self, data):
@@ -60,7 +60,8 @@ class HumanFollower:
         if cmd_vel.linear.x == 0 and cmd_vel.angular.z == 0:
             self.pub_stop_vel()
 
-        rospy.loginfo(f"distance : {distance}")
+        # rospy.loginfo(f"distance : {distance}")
+        rospy.loginfo(f"angle_deg : {angle_radians * 180 / (2 * math.pi)}")
 
     def aruco_tf_callback(self, data):
         self.current_aruco_tf = data
